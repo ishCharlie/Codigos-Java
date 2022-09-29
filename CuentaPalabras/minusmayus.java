8d0130b9e78646bd5b32626271a6ad00ff02b164
Carlos Manriquez <110861748+ishCharlie@users.noreply.github.com>
2022-09-29 12:44:51 -0500

    Add files via upload

---
+++ CuentaPalabras/minusmayus.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class minusmayus
+{
+public static void main(String[] args) {
+Scanner lector = new Scanner (System.in);
+String cadena = "";
+char [] Arraycadena;
+char caracter;
+int contador=0;
+
+System.out.println("Ingrese un texto: ");
+cadena = lector.nextLine();
+Arraycadena = cadena.toCharArray();
+for (int i = 0; i < Arraycadena.length; i++){
+    caracter = Arraycadena [i];
+
+    for (int j = 0; j <Arraycadena.length; j++){
+        if (Arraycadena[j] == caracter) {
+            contador++;
+
+        }
+    }
+
+    System.out.println(Arraycadena[i]+ ""+ contador);
+    contador = 0;
+
+}
+}
+
+}
